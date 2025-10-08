49a4078177a412b616817886525468a772c56382
wasabina67 <wasabina67@gmail.com>
2025-10-08 18:14:02 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,14 @@ FROM ruby:3.2
 
 WORKDIR /app
 
+RUN apt-get update && apt-get install -y wget unzip && rm -rf /var/lib/apt/lists/*
+
+RUN mkdir -p /usr/share/fonts/noto && \
+    cd /tmp && \
+    wget https://github.com/notofonts/noto-cjk/releases/download/Sans2.004/02_NotoSansCJK-TTF-VF.zip && \
+    unzip 02_NotoSansCJK-TTF-VF.zip -d /usr/share/fonts/noto/ && \
+    rm 02_NotoSansCJK-TTF-VF.zip
+
 COPY Gemfile ./
 RUN bundle install
 
--- generate_pdf.rb
+++ generate_pdf.rb
@@ -1,19 +1,35 @@
 require 'prawn'
+require 'prawn/table'
+
+font_dir = '/usr/share/fonts/noto'
+noto_sans_vf = Dir.glob("#{font_dir}/**/NotoSansCJKjp-VF.ttf").first
 
 Prawn::Document.generate('output/sample.pdf') do
-  text 'Hello, Prawn!', size: 30, style: :bold
+  if noto_sans_vf && File.exist?(noto_sans_vf)
+    font_families.update(
+      'NotoSansCJKjp' => {
+        normal: noto_sans_vf,
+        bold: noto_sans_vf,
+        italic: noto_sans_vf
+      }
+    )
+    font 'NotoSansCJKjp'
+  end
+
+  text 'こんにちは、Prawn!', size: 30, style: :bold
 
   move_down 20
-  text 'This is a minimal Prawn sample application.'
+  text 'これは日本語に対応したPrawnのサンプルアプリケーションです。'
 
   move_down 20
-  text 'Prawn makes it easy to generate PDFs with Ruby.', size: 12
+  text 'PrawnとNoto Sans JPフォントを使用してPDFを簡単に生成できます。', size: 12
 
   move_down 30
   stroke_horizontal_rule
 
   move_down 20
-  text "Generated at: #{Time.now}", size: 10, style: :italic
+  jst_time = Time.now.getlocal('+09:00')
+  text "生成日時: #{jst_time.strftime('%Y-%m-%d %H:%M:%S')}", size: 10, style: :italic
 end
 
 puts 'PDF generated successfully: output/sample.pdf'
